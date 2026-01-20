24435182334dc26c6e8a81aba1e915b5b2306a84
jakubprusakiewicz <jpr@200co.com>
2026-01-20 10:21:16 +0100

    adjust integration test to github account aws policies

--- tests/linked_service/integration/test_connect.py
+++ tests/linked_service/integration/test_connect.py
@@ -8,9 +8,15 @@ pytestmark = pytest.mark.integration
 
 
 def test_connect_creates_session_and_clients():
-    access_key_id = os.environ.get("AWS-ACCESS-KEY-ID")
-    access_key_secret = os.environ.get("AWS-SECRET-ACCESS-KEY")
+    # given
+    access_key_id = os.environ.get("AWS_ACCESS_KEY_ID") or os.environ.get("AWS-ACCESS-KEY-ID")
+    access_key_secret = os.environ.get("AWS_SECRET_ACCESS_KEY") or os.environ.get("AWS-SECRET-ACCESS-KEY")
     settings = AWSLinkedServiceSettings(access_key_id=access_key_id, access_key_secret=access_key_secret)
     aws_linked_service = AWSLinkedService(settings=settings)
-    result = aws_linked_service.test_connection()
-    assert result == (True, "Connection successfully tested")
+    # when
+    connection = aws_linked_service.connect()
+    result = connection.client("sts").get_caller_identity()
+    # then
+    assert "Account" in result
+    assert "UserId" in result
+    assert "Arn" in result
