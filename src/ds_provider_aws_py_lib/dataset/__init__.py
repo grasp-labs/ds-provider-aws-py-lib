f01344a681dfee7286768aea546279caff91da50
jakubprusakiewicz <jpr@200co.com>
2026-04-17 14:11:49 +0200

    docs: update module docstring with usage examples for AWSLinkedService and S3Dataset

--- src/ds_provider_aws_py_lib/dataset/__init__.py
+++ src/ds_provider_aws_py_lib/dataset/__init__.py
@@ -7,7 +7,29 @@ AWS Dataset
 This module implements a dataset for AWS (S3)
 
 Example:
-    ... # todo
+   >>> linked_service = AWSLinkedService(
+   ...     id=UUID("00000000-0000-0000-0000-000000000000"),
+   ...     name="test-name",
+   ...     version="1.0.0",
+   ...     settings=AWSLinkedServiceSettings(
+   ...         account_id="...",
+   ...         access_key_id="...",
+   ...         access_key_secret="...",
+   ...         region="us-east-1",
+   ...     ),
+   ... )
+   >>> dataset = S3Dataset(
+   ...     id=UUID("00000000-0000-0000-0000-000000000001"),
+   ...     name="test-s3-dataset",
+   ...     version="1.0.0",
+   ...     settings=S3DatasetSettings(
+   ...         bucket="test-package",
+   ...         key="test3/*.csv",
+   ...     ),
+   ...     linked_service=linked_service,
+   ... )
+   >>> dataset.read()
+   >>> data = dataset.output
 """
 
 from .s3 import S3Dataset, S3DatasetSettings
